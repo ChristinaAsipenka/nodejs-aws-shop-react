ef5002231d2f86eaf2b4270716783c1a8e413900
Christina Asipenka <krystinabutko@gmail.com>
2024-07-05 22:05:00 +0200

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://1pcdcu2oob.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://8dgw3n1a7g.execute-api.eu-west-1.amazonaws.com/prod",
+  import: "https://m4bovxv7ah.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
